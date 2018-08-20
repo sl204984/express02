3b63ae99b37430563be1d4eb801a1f79087cfff8
sl204984 <17798520003@163.com>
2018-08-20 09:22:44 +0800

    首页

--- routers/home-page/index.js
+++ routers/home-page/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const {
     pageSize,
     pageNum,
